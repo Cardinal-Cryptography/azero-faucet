8d3b45d6a3a0615dc8d3c64cc0e9c44a2f80e71d
Mateusz Pęczkowski <mateusz@peczis.pl>
2021-07-19 14:48:09 +0200

    add recaptcha

--- static/js/main.js
+++ static/js/main.js
@@ -55,3 +55,11 @@ $(() => {
     });
   });
 });
+
+window.onload = function() {
+  var $recaptcha = document.querySelector('#g-recaptcha-response');
+
+  if($recaptcha) {
+    $recaptcha.setAttribute("required", "required");
+  }
+};
