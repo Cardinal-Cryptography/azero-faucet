9daf30710ba242c75a0aa8589a99ad7d3f0c1e6e
timorl <tymorl@gmail.com>
2021-07-20 11:23:17 +0200

    Make recaptcha work

--- src/types.ts
+++ src/types.ts
@@ -35,7 +35,8 @@ export type EnvNamePage = 'BACKEND_URL'|
  'DRIP_AMOUNT' |
  'PAGE_PORT' |
  'COOLDOWN' |
- 'NETWORK_UNIT'
+ 'NETWORK_UNIT' |
+ 'GOOGLE_CAPTCHA_PRIVATE'
 ;
 
 export interface EnvOpt {
